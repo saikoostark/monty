7e555d74828787edbdb2343faecfd84952609654
Saikoo_Stark <alwkylsyd87@example.com>
2023-08-24 11:28:05 +0300

    fix spaces

--- execute.c
+++ execute.c
@@ -12,11 +12,12 @@ void execute(stack_t **stack, char **arrayOfLines, int lineNumber)
 	char *str = strdup(arrayOfLines[lineNumber - 1]);
 	char *opcode = strtok(str, " ");
 	instruction_t opst[] = {
-		{"push", push}, {"pall", pall}, {"pint", pint}, 
-		{"pop", pop}, {"swap", swap}, {"add", add}, 
-		{"nop", nop}, {"sub", sub}, {"div", div}, {"mul", mul}, 
-		{"mod", mod}, {"pchar", pchar}, {"pstr", pstr}, 
-		{"rotl", rotl}, {"rotr", rotr}, {NULL, NULL}};
+		{"push", push}, {"pall", pall}, {"pint", pint},
+		{"pop", pop}, {"swap", swap}, {"add", add},
+		{"nop", nop}, {"sub", sub}, {"div", div},
+		{"mul", mul}, {"mod", mod}, {"pchar", pchar},
+		{"pstr", pstr}, {"rotl", rotl}, {"rotr", rotr},
+		{NULL, NULL}};
 
 	/*
 	{"queue", queue},
