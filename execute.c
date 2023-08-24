e1ef38c37befa909f9dc6890d5e16d54834600d6
Saikoo_Stark <alwkylsyd87@example.com>
2023-08-24 11:27:05 +0300

    fix betty

--- execute.c
+++ execute.c
@@ -12,12 +12,16 @@ void execute(stack_t **stack, char **arrayOfLines, int lineNumber)
 	char *str = strdup(arrayOfLines[lineNumber - 1]);
 	char *opcode = strtok(str, " ");
 	instruction_t opst[] = {
-		{"push", push}, {"pall", pall}, {"pint", pint}, {"pop", pop}, {"swap", swap}, {"add", add}, {"nop", nop}, {"sub", sub}, {"div", div}, {"mul", mul}, {"mod", mod}, {"pchar", pchar}, {"pstr", pstr}, {"rotl", rotl}, {"rotr", rotr},
-		/*
-		{"queue", queue},
-		{"stack", stack},
-		*/
-		{NULL, NULL}};
+		{"push", push}, {"pall", pall}, {"pint", pint}, 
+		{"pop", pop}, {"swap", swap}, {"add", add}, 
+		{"nop", nop}, {"sub", sub}, {"div", div}, {"mul", mul}, 
+		{"mod", mod}, {"pchar", pchar}, {"pstr", pstr}, 
+		{"rotl", rotl}, {"rotr", rotr}, {NULL, NULL}};
+
+	/*
+	{"queue", queue},
+	{"stack", stack},
+	*/
 	int i = 0, flag = 1, error = 0;
 
 	for (i = 0; opst[i].opcode != NULL; i++)
